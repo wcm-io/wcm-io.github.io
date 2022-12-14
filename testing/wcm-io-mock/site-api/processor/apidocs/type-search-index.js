683d615a4958ef4ed893cf13bed61e3bb5acb25e
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2022-12-14 08:12:12 +0000

    Maven site: wcm-io/io.wcm.testing.wcm-io-mock.site-api.processor

---
+++ testing/wcm-io-mock/site-api/processor/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.wcm.testing.mock.wcmio.siteapi.processor","l":"ContextPlugins"},{"p":"io.wcm.testing.mock.wcmio.siteapi.processor","l":"MockProcessorRequestContext"},{"p":"io.wcm.testing.mock.wcmio.siteapi.processor","l":"MockSiteApiConfiguration"}]
