1a937478e7495a1fced1204a6871f33dd73a2e2f
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2022-12-16 15:11:15 +0000

    Maven site: wcm-io/io.wcm.maven.plugins.i18n-maven-plugin

---
+++ tooling/maven/plugins/i18n-maven-plugin/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.maven.plugins.i18n"},{"l":"io.wcm.maven.plugins.i18n_maven_plugin"},{"l":"io.wcm.maven.plugins.i18n.readers"}]
