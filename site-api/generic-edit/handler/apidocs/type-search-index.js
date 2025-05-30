fcd3b1bb670bb001b5bc1ce9a6fa6ab229c92b33
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-05-30 12:44:09 +0000

    Maven site: wcm-io/io.wcm.site-api.generic-edit.handler

---
+++ site-api/generic-edit/handler/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.wcm.siteapi.genericedit.handler.link","l":"SiteApiInternalLinkInheritGenericEditSelectorLinkPreProcessor"}]
