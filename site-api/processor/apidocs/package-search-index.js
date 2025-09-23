734da2d30ec4903aea61677bfb3dae4929e71e4f
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:23:01 +0000

    Maven site: wcm-io/io.wcm.site-api.processor

--- site-api/processor/apidocs/package-search-index.js
+++ site-api/processor/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.siteapi.processor"},{"l":"io.wcm.siteapi.processor.caconfig"},{"l":"io.wcm.siteapi.processor.url"},{"l":"io.wcm.siteapi.processor.util"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.siteapi.processor"},{"l":"io.wcm.siteapi.processor.caconfig"},{"l":"io.wcm.siteapi.processor.url"},{"l":"io.wcm.siteapi.processor.util"}];updateSearchResults();
