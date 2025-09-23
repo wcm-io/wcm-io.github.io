ad55cb4c74ffc06886dcc6692fd78899ffce6bb3
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:19:44 +0000

    Maven site: wcm-io/io.wcm.caconfig.editor

--- caconfig/editor/caconfig/editor/bundle/apidocs/package-search-index.js
+++ caconfig/editor/caconfig/editor/bundle/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.caconfig.editor"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.caconfig.editor"}];updateSearchResults();
