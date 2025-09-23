31f44434297bc746ea1755f514438c62455bee17
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:47:42 +0000

    Maven site: wcm-io/io.wcm.site-api.generic-edit.handler

--- site-api/generic-edit/handler/apidocs/package-search-index.js
+++ site-api/generic-edit/handler/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.siteapi.genericedit.handler.link"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.siteapi.genericedit.handler.link"}];updateSearchResults();
