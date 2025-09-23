32d7d0843675e108bafbbc3f89ad215d17db3eb2
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:25:23 +0000

    Maven site: wcm-io/io.wcm.testing.wcm-io-mock.wcm

--- testing/wcm-io-mock/wcm/apidocs/package-search-index.js
+++ testing/wcm-io-mock/wcm/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.testing.mock.wcmio.wcm"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.testing.mock.wcmio.wcm"}];updateSearchResults();
--- testing/wcm-io-mock/wcm/apidocs/type-search-index.js
+++ testing/wcm-io-mock/wcm/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.wcm.testing.mock.wcmio.wcm","l":"ContextPlugins"},{"p":"io.wcm.testing.mock.wcmio.wcm","l":"MockInstanceType"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.wcm.testing.mock.wcmio.wcm","l":"ContextPlugins"},{"p":"io.wcm.testing.mock.wcmio.wcm","l":"MockInstanceType"}];updateSearchResults();
