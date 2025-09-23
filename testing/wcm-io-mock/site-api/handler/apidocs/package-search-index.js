8235566ee8e50f3dc81d2b37622c5f2137338372
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:24:37 +0000

    Maven site: wcm-io/io.wcm.testing.wcm-io-mock.site-api.handler

--- testing/wcm-io-mock/site-api/handler/apidocs/package-search-index.js
+++ testing/wcm-io-mock/site-api/handler/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.testing.mock.wcmio.siteapi.handler"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.testing.mock.wcmio.siteapi.handler"}];updateSearchResults();
