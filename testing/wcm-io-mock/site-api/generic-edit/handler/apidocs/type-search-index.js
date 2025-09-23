f65cd706121866362f3737b7cabdb274989388bc
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:24:17 +0000

    Maven site: wcm-io/io.wcm.testing.wcm-io-mock.site-api.generic-edit.handler

--- testing/wcm-io-mock/site-api/generic-edit/handler/apidocs/type-search-index.js
+++ testing/wcm-io-mock/site-api/generic-edit/handler/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.wcm.testing.mock.wcmio.siteapi.genericedit.handler","l":"ContextPlugins"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.wcm.testing.mock.wcmio.siteapi.genericedit.handler","l":"ContextPlugins"}];updateSearchResults();
