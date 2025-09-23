3a935f4657743d0fdd774289f68d0eba86aeb5c8
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-09-23 10:20:17 +0000

    Maven site: wcm-io/io.wcm.tooling.commons.content-package-builder

--- tooling/commons/content-package-builder/apidocs/package-search-index.js
+++ tooling/commons/content-package-builder/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.wcm.tooling.commons.contentpackagebuilder"},{"l":"io.wcm.tooling.commons.contentpackagebuilder.element"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.wcm.tooling.commons.contentpackagebuilder"},{"l":"io.wcm.tooling.commons.contentpackagebuilder.element"}];updateSearchResults();
