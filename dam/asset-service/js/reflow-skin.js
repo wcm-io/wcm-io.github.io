505431275cbb6cc726ba5198088583cc1de7f4d7
wcmio-site-deploy <78385463+wcmio-site-deploy@users.noreply.github.com>
2025-10-07 14:25:48 +0000

    Maven site: wcm-io/io.wcm.dam.asset-service

--- dam/asset-service/js/reflow-skin.js
+++ dam/asset-service/js/reflow-skin.js
@@ -16,9 +16,7 @@
     // Note: only run if `hljs` exists
     if (typeof hljs != 'undefined')
     {
-      // classic encoding with <div class="source"><pre></pre></div>
-      // and HTML5 version with <pre><code></code></pre>
-      $('div.source pre, pre code, pre.source').each(function(i, e) {hljs.highlightBlock(e)});
+      $('pre code').each(function(i, e) {hljs.highlightElement(e)});
     }
 
   })
